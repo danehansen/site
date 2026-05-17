3b4119622d76bd1d646e94e49b7b86ce5cd11b8b
Dane Hansen <dane@danehansen.com>
2026-05-17 13:17:01 -0700

    remove php from oregon

--- docs/archives/oregon/php/js.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$arr = include $_SERVER["DOCUMENT_ROOT"].'/min/groupsConfig.php';
-	foreach ($arr['js'] as $script) {
-		$pieces = explode("//", $script);
-		echo "<script src=\"/".$pieces[1]."\"></script>";
-	} 
-
-?>
-
